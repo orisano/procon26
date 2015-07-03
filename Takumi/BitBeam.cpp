5b3c10db43682f7075d49521d2aa20f3c32864f9
Orisano <weekendsummer.endless@gmail.com>
2015-07-03 19:52:45 +0900

    fix bug

--- Takumi/BitBeam.cpp
+++ Takumi/BitBeam.cpp
@@ -8,26 +8,26 @@
 #include "../CacheTile.hpp"
 #include "ZobristHash.hpp"
 #include "../../../orliv/benchmark.hpp"
+#include "../external/cmdline/cmdline.h"
 
 // #define BEAM_BENCH
 
 namespace {
 const int dx[] = {0, 0, 1, -1};
 const int dy[] = {1, -1, 0, 0};
-const int BEAM_WIDTH = 2000;
+// const int BEAM_WIDTH = 3000;
 const int ONE_STEP = 200;
 const int SIZE = 32;
-const int tri[] = {-1, -1, 3, 9, 27};
+const int pn[] = {0, 1, 3, 7, 13};
 
 using procon26::board::BitBoard;
 
-int dfs(int data[SIZE][SIZE], int x, int y)
-{
+int dfs(int data[SIZE][SIZE], int x, int y) {
     if (0 > x || x >= SIZE || 0 > y || y >= SIZE) return 0;
     if (data[y][x] == 1) return 0;
     data[y][x] = 1;
     int sum = 1;
-    for (int d = 0; d < 4; d++){
+    for (int d = 0; d < 4; d++) {
         sum += dfs(data, x + dx[d], y + dy[d]);
     }
     return sum;
@@ -36,21 +36,21 @@ int dfs(int data[SIZE][SIZE], int x, int y)
 
 int evalBoard(const BitBoard &board) {
     int vis[SIZE][SIZE] = {};
-    int data[SIZE][SIZE];
     for (int y = 0; y < SIZE; y++) {
         for (int x = 0; x < SIZE; x++) {
-            data[y][x] = 0;
-            if (!board.at(x, y)) continue;
-            data[y][x] = 1;
+            if (board.at(x, y)) continue;
             for (int d = 0; d < 4; d++) {
                 int nx = x + dx[d], ny = y + dy[d];
-                if (!board.inBounds(nx, ny)) continue;
-                if (board.at(nx, ny) || (board.initial[ny] >> nx & 1)) continue;
+                if (!board.inBounds(nx, ny)) {
+                    vis[ny][nx]++;
+                    continue;
+                }
+                if (board.at(nx, ny) == 0) continue;
                 vis[ny][nx]++;
             }
         }
     }
-
+    /*
     std::vector<int> vs;
     double avg = 0;
     for (int y = 0; y < SIZE; y++){
@@ -61,9 +61,12 @@ int evalBoard(const BitBoard &board) {
             vs.push_back(rv);
         }
     }
+    */
+    /*
     const int N = vs.size();
     const int MIN_V = *std::min_element(vs.begin(), vs.end());
     const int MAX_V = *std::max_element(vs.begin(), vs.end());
+    */
     /*
     avg /= N;
     double variance = 0;
@@ -75,26 +78,32 @@ int evalBoard(const BitBoard &board) {
     int density = 0;
     for (int y = 0; y < SIZE; y++) {
         for (int x = 0; x < SIZE; x++) {
-            density += tri[vis[y][x]];
+            density += pn[vis[y][x]];
         }
     }
-    return (board.blanks() + density) + 32 * N + MAX_V / MIN_V;
+    return board.blanks() + density;
 }
 
-void dumpBoard(const BitBoard &board) {
-    std::puts("----------------");
+int getColor(int c) {
+    if (c == 0) return 47;
+    if (c == 1) return 40;
+    return (c - 2) % 6 + 41;
+}
+
+void dumpBoard(const BitBoard &board, bool number = false) {
+    puts("----------------");
     for (int i = 0; i < 32; i++) {
-        auto init = board.initial[i];
-        auto curr = board.data[i];
         for (int j = 0; j < 32; j++) {
-            std::printf("%4d", ((init >> j) & 1) * 2 | ((curr >> j) & 1));
+            auto c = board.at(j, i);
+            if (number) printf("%4d", c);
+            else printf("\x1b[%dm  \x1b[49m", getColor(c));
         }
-        std::puts("");
+        puts("");
     }
-    std::printf("blanks: %d\n", board.blanks());
-    std::puts("----------------");
+    printf("blanks: %d\n", board.blanks());
+    puts("----------------");
 }
-}
+
 
 struct EBoard : public BitBoard {
     using Derived = BitBoard;
@@ -121,19 +130,22 @@ struct EBoard : public BitBoard {
   private:
     std::uint64_t used_[4];
 };
-
+}
 
 namespace procon26 {
 namespace takumi {
 
-Answer BitBeam::solve(const Home &home, int millisec) {
+Answer BitBeam::solve(const Home &home, int millisec, cmdline::parser& parser) {
     using tile::BitTile;
+
+    const int BEAM_WIDTH = parser.get<int>("beam_width");
+
     BitBoard initial(home.board);
-    std::vector<CacheTile<BitTile> > tiles;
-    int id = 2;
+    std::vector <CacheTile<BitTile>> tiles;
+    int id = 0;
     for (auto &tile : home.tiles) tiles.emplace_back(BitTile(tile)), tiles.back().fill(id++);
 
-    std::vector<EBoard> beam;
+    std::vector <EBoard> beam;
     beam.emplace_back(initial);
     /*
      * あとで重複削除入れる
@@ -142,13 +154,14 @@ Answer BitBeam::solve(const Home &home, int millisec) {
     orliv::ZobristHash<std::uint64_t, 32, 32, 2> zb;
 
     auto start = std::chrono::high_resolution_clock::now();
-    std::vector<EBoard> nxt;
+    std::vector <EBoard> nxt;
     nxt.reserve(700000);
-    std::set<std::uint64_t> vis;
+    std::set <std::uint64_t> vis;
     auto best = initial;
     int tile_id = 0;
-    while ((tile_id < tiles.size() && beam.size())){ // std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() + ONE_STEP < millisec) {
-        printf("tile_id: %d / %lu\n", tile_id, tiles.size());
+    while ((tile_id < tiles.size() &&
+            beam.size())) { // std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() + ONE_STEP < millisec) {
+        printf("::tile_id: %d / %lu\n", tile_id, tiles.size());
         dumpBoard(beam[0]);
         //dumpBoard(best);
         nxt.clear();
@@ -156,36 +169,36 @@ Answer BitBeam::solve(const Home &home, int millisec) {
         int beam_count = 0;
         benchmark("next beam generate") {
             for (const auto &b : beam) {
-                if (beam_count++ < (BEAM_WIDTH / 10)) nxt.push_back(b);
+                if (beam_count++ < BEAM_WIDTH) nxt.push_back(b);
 #ifdef BEAM_BENCH
                 benchmark("beam") {
 #endif
-                    auto tile = tiles[tile_id];
-                    for (int y = -7; y < 32; y++) {
-                        for (int x = -7; x < 32; x++) {
-                            for (int r = 0; r < 4; r++, tile.rotate()) {
-                                if (!b.canPut(tile.value(), x, y)) continue;
-                                EBoard nb = b;
-                                nb.put(tile.value(), x, y);
-                                nb.useTile(tile_id);
-                                auto hashv = zb.hash(nb);
-                                if (vis.count(hashv)) continue;
-                                vis.insert(hashv);
-                                nxt.push_back(nb);
-                            }
-                            tile.reverse();
-                            for (int r = 0; r < 4; r++, tile.rotate()) {
-                                if (!b.canPut(tile.value(), x, y)) continue;
-                                EBoard nb = b;
-                                nb.put(tile.value(), x, y);
-                                nb.useTile(tile_id);
-                                auto hashv = zb.hash(nb);
-                                if (vis.count(hashv)) continue;
-                                vis.insert(hashv);
-                                nxt.push_back(nb);
-                            }
+                auto& tile = tiles[tile_id];
+                for (int y = -7; y < 32; y++) {
+                    for (int x = -7; x < 32; x++) {
+                        for (int r = 0; r < 4; r++, tile.rotate()) {
+                            if (!b.canPut(tile.value(), x, y)) continue;
+                            EBoard nb = b;
+                            nb.put(tile.value(), x, y);
+                            nb.useTile(tile_id);
+                            auto hashv = zb.hash(nb);
+                            if (vis.count(hashv)) continue;
+                            vis.insert(hashv);
+                            nxt.emplace_back(nb);
+                        }
+                        tile.reverse();
+                        for (int r = 0; r < 4; r++, tile.rotate()) {
+                            if (!b.canPut(tile.value(), x, y)) continue;
+                            EBoard nb = b;
+                            nb.put(tile.value(), x, y);
+                            nb.useTile(tile_id);
+                            auto hashv = zb.hash(nb);
+                            if (vis.count(hashv)) continue;
+                            vis.insert(hashv);
+                            nxt.emplace_back(nb);
                         }
                     }
+                }
 #ifdef BEAM_BENCH
                 }
 #endif
@@ -214,9 +227,15 @@ Answer BitBeam::solve(const Home &home, int millisec) {
         std::swap(beam, nxt);
         tile_id++;
     }
-    dumpBoard(best);
+    dumpBoard(best, true);
     return Answer();
 }
 
+cmdline::parser BitBeam::getParser() {
+    cmdline::parser parser;
+    parser.add<int>("beam_width", 'b', "beam width", false, 1000);
+
+    return parser;
+}
 }
 }
